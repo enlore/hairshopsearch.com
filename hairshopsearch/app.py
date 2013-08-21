30352ea35ff24c4f7915a9d24c0826cdf7ce94e8
enlore <n.e.lorenson@gmail.com>
2013-08-21 16:22:15 -0500

    Ignore config

---
+++ hairshopsearch/app.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+import os
+from flask import Flask
+
+def create_app(config=None):
+    """App factory, optionally passed a config file path from Manager"""
+    app = Flask('hairshopsearch')
+    configure_app(app, config)
+    bootstrap_blueprints(app)
+    return app
+
+def configure_app(app, config):
+    """Use local production config or passed in dev config via Manager"""
+    if config is None:
+        config = os.path.join(app.root_path, 'production.cfg')
+
+    app.config.from_pyfile(config)
+
+def bootstrap_blueprints(app):
+    """Register blueprints on app."""
+    for bp in app.config['BLUEPRINTS']:
+        app.register_blueprint(bp)
+
