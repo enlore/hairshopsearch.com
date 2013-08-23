8c80667a5eed395d9b33f3d2545fad5871f84860
enlore <n.e.lorenson@gmail.com>
2013-08-23 17:21:28 -0500

    Use mkvirtualenv in fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,31 @@
 from fabric.api import *
 
-project = 'Hair Shop Search'
+project = 'hairshopsearch'
 
-env.user = "no"
-env.hosts = ["hairshopsearch.com"]
+env.user = 'no'
+env.hosts = ['hairshopsearch.com']
 
 def setup():
     """Set up venv"""
 
-    local("virtualenv venv")
-    activate_this = "venv/bin/activate_this.py"
-    execfile(activate_this, dict(__file__=activate_this))
-    local("python setup.py install")
+    local('mkvirtualenv %s' % project)
+    local('workon %s' % project)
+    local('python setup.py install')
     reset()
 
 def reset():
     """Reset local dev environment"""
 
-    local("python manage.py init_db")
+    local('python manage.py init_db')
 
 def d():
     """Run app in dev"""
 
     reset()
-    local("python manage.py -c dev.cfg run")
+    local('python manage.py -c dev.cfg run')
 
 def pack():
-    local("python setup.py sdist --formats=gztar")
+    local('python setup.py sdist --formats=gztar')
 
 def deploy():
     pass
