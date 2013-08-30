5fe0a660c37a4ac3f4915a2b98f23e9791c15f7e
enlore <n.e.lorenson@gmail.com>
2013-08-30 16:15:07 -0500

    Register links and records

--- hairshopsearch/frontend/views.py
+++ hairshopsearch/frontend/views.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, render_template
 from ..stylist.models import Stylist
 from ..salonmanager.models import SalonManager
+from ..customer.models import Customer
 
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
@@ -9,4 +10,6 @@ frontend = Blueprint('frontend', __name__, template_folder='templates')
 def index():
     stylists = Stylist.query.all()
     s_ms = SalonManager.query.all()
-    return render_template('index.html',stylists=stylists, s_ms = s_ms)
+    customers = Customer.query.all()
+    return render_template('index.html',stylists=stylists, s_ms = s_ms,
+            customers=customers)
