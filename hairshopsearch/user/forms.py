09866276decee562f67237eef3868c49b76a3076
enlore <n.e.lorenson@gmail.com>
2013-08-30 16:16:36 -0500

    Reorder email field

--- hairshopsearch/user/forms.py
+++ hairshopsearch/user/forms.py
@@ -4,9 +4,9 @@ from wtforms.validators import Required, Email
 
 
 class UserForm(Form):
-    email               = TextField(u'Email:', [Required(), Email()])
     first_name          = TextField(u'First Name:', [Required()])
     last_name           = TextField(u'Last Name:')
+    email               = TextField(u'Email:', [Required(), Email()])
     password            = PasswordField(u'Password:', [Required()])
     confirm_password    = PasswordField(u'Confirm Password:', [Required()])
 
