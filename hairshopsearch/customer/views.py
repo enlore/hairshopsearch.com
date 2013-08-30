94dae206b75e4ad6139cf04a41bf7c60ada1de50
enlore <n.e.lorenson@gmail.com>
2013-08-30 16:14:52 -0500

    Customer registration template and db commit

--- hairshopsearch/customer/views.py
+++ hairshopsearch/customer/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for
 from .models import Customer
 from .forms import CustomerForm
+from ..user.models import User
+from ..extensions import db
 
 customer = Blueprint('customer', __name__, url_prefix='/customer', 
         template_folder='templates')
@@ -13,6 +15,14 @@ def index():
 def register():
     form = CustomerForm()
     if form.validate_on_submit():
+        customer = Customer()
+        form.populate_obj(customer)
+        
+        customer.user = User()
+        form.populate_obj(customer.user)
+
+        db.session.add(customer)
+        db.session.commit()
 
         return redirect(url_for('frontend.index'))
     return render_template('customer/register.html', form=form)
