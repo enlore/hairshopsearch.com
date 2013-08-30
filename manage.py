00e5929ad801a9d6e18a84df06f338ce121f93d8
enlore <n.e.lorenson@gmail.com>
2013-08-30 12:26:15 -0500

    Init db in manager

--- manage.py
+++ manage.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager, Server, Command
 from hairshopsearch import create_app
+from hairshopsearch.extensions import db
 
-manager = Manager(create_app)
+manager = Manager(create_app())
 
-@manager.command
-def init_db():
+class Init_db(Command):
     """Initializes a fresh db"""
-    print 'Initdbing!'
+    def run(self):
+        manager.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/hairshopsearch.db'
+        manager.app.config['SQLALCHEMY_ECHO'] = True
+        db.drop_all(app=manager.app)
+        db.create_all(app=manager.app)
 
+manager.add_command('init_db', Init_db())
 manager.add_command('run', Server(host='127.0.0.1',port=9007))
 manager.add_option(
         '-c', '--config', dest='config',
