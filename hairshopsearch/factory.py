716983c8fcece6398b873ceeb439eaff6ac6b22f
enlore <n.e.lorenson@gmail.com>
2013-09-22 15:48:07 -0500

    Config app

--- hairshopsearch/factory.py
+++ hairshopsearch/factory.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from config import Config
 
 def create_app(config=None):
     app = Flask('hairshopsearch')
+    config_app(app, config)
     return app
+
+def config_app(app, config):
+    app.config.from_obj(Config)
+    if config not None:
+        app.config.from_pyfile(config)
