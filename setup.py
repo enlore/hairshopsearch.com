27c8e712fb21c2f7b39d27a0d4dd4dcfbcbaa6bd
enlore <n.e.lorenson@gmail.com>
2013-09-22 15:46:18 -0500

    Setup.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="Hair Shop Search",
-    version="0.0.1",
+    name="hairshopsearch",
+    version="0.3.0",
     long_description=__doc__,
     packages=["hairshopsearch"],
     include_package_data=True,
     zip_safe=False,
     install_requires=[]
-        )
+)
