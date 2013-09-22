424696a193570da9ee342ccddfe5c8020a47224e
enlore <n.e.lorenson@gmail.com>
2013-09-22 15:52:46 -0500

    Server, Shell

--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
-from flask.ext.script import Manager, Server, Shell
+#!/home/no/.venvs/hairshopsearch/bin/python
+from flask.ext.script import Manager, Server, Shell 
 from hairshopsearch.factory import create_app
 
 m = Manager(create_app)
 
+def _make_context():
+    return dict(
+           app=create_app(),
+            )
+
 m.add_command('run', Server(port='9016'))
+m.add_command('shell', Shell(make_context=_make_context))
 
 if __name__ == '__main__':
     m.run()
