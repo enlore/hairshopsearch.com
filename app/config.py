1e4aeaa2943a23b41416e478c5a28e0bcca6ce80
enlore <n.e.lorenson@gmail.com>
2013-09-24 17:48:12 -0500

    Config sec and mail

--- app/config.py
+++ app/config.py
@@ -3,9 +3,27 @@ class Default():
 
 
 class DevConfig(Default):
+    FILE_LOG = '/tmp/hairshopsearch.info.log'
     SECRET_KEY = 'dev key'
     DEBUG = True
+    DEBUG_TB_PROFILER_ENABLED = True
 
+    WTF_CSRF_ENABLED = True
+
+    MAIL_SERVER = 'smtp.mailgun.org'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'postmaster@hairshopsearch.com'
+    MAIL_PASSWORD = '4potz4gocm60'
+
+    SECURITY_REGISTERABLE = True
+    SECURITY_SEND_REGISTER_EMAIL = True
+    SECURITY_EMAIL_SENDER = 'services@hairshopsearch.com'
+    SECURITY_CONFIRMABLE = True
+    SECURITY_TRACKABLE = True
+    SECURITY_PASSWORD_HASH = 'bcrypt'
+    SECURITY_RECOVERABLE = True
+    SECURITY_CHANGEABLE = True
 
 class Config(DevConfig):
     pass
