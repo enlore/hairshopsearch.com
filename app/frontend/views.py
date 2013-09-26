6fc37098dde6eb396ba89eba657c24a1efe7903e
enlore <n.e.lorenson@gmail.com>
2013-09-26 17:02:45 -0500

    Consumer profile

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from flask import Blueprint, render_template
 from sqlalchemy import or_
-from ..user.models import Provider
+from ..user.models import Provider, Consumer
 
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
@@ -14,3 +14,8 @@ def provider(provider_name):
     provider = Provider.query.filter(or_(Provider.business_name==provider_name, Provider.id==provider_name))\
                 .first()
     return render_template('frontend/provider.html', provider=provider)
+
+@frontend.route('/consumer/<consumer_id>')
+def consumer(consumer_id):
+    consumer = Consumer.query.filter(Consumer.id==consumer_id).first()
+    return render_template('frontend/consumer.html', consumer=consumer)
--- app/user/models.py
+++ app/user/models.py
@@ -15,8 +15,11 @@ class Role(db.Model, RoleMixin):
 
 
 class User(db.Model, UserMixin):
-    id              = db.Column(db.Integer, primary_key=True)
+    id                  = db.Column(db.Integer, primary_key=True)
     provider_id         = db.Column(db.Integer, db.ForeignKey('provider.id'))
+    consumer_id         = db.Column(db.Integer, db.ForeignKey('consumer.id'))
+    first_name          = db.Column(db.String)
+    last_name           = db.Column(db.String)
     email               = db.Column(db.String(), unique=True)
     password            = db.Column(db.String())
     active              = db.Column(db.Boolean())
@@ -63,6 +66,7 @@ class Photo(db.Model):
     url                 = db.Column(db.Text)
     provider_id         = db.Column(db.Integer, db.ForeignKey('provider.id'))
     gallery_id          = db.Column(db.Integer, db.ForeignKey('gallery.id'))
+    consumer_id         = db.Column(db.Integer, db.ForeignKey('consumer.id'))
 
 
 class Gallery(db.Model):
@@ -71,11 +75,32 @@ class Gallery(db.Model):
     photos              = db.relationship('Photo', backref='gallery')
 
 
+class Article(db.Model):
+    id                  = db.Column(db.Integer, primary_key=True)
+    consumer_id         = db.Column(db.Integer, db.ForeignKey('consumer.id'))
+    body                = db.Column(db.Text)
+    title               = db.Column(db.String)
+
+
+class Product(db.Model):
+    id                  = db.Column(db.Integer, primary_key=True)
+    name                = db.Column(db.String)
+    description         = db.Column(db.String)
+    provider_id         = db.Column(db.Integer, db.ForeignKey('provider.id'))
+    consumer_id         = db.Column(db.Integer, db.ForeignKey('consumer.id'))
+
+
+consumers_providers = db.Table('consumers_providers',
+        db.Column('consumer_id', db.Integer, db.ForeignKey('consumer.id')),
+        db.Column('provider_id', db.Integer, db.ForeignKey('provider.id'))
+        )
+
 class Provider(db.Model):
     id                  = db.Column(db.Integer, primary_key=True)
-    user                = db.relationship('User', backref='provider', 
+    user                = db.relationship('User', backref='provider',
                             uselist=False)
     business_name       = db.Column(db.String)
+    phone               = db.Column(db.String)
     bio                 = db.Column(db.Text)
     fb_url              = db.Column(db.String)
     twitter_url         = db.Column(db.String)
@@ -83,16 +108,31 @@ class Provider(db.Model):
     hours               = db.Column(db.PickleType)
     payment_methods     = db.Column(db.String)
     links               = db.Column(db.String)
-    
+
     address             = db.relationship('Address', uselist=False,
                             backref=db.backref('provider'))
-    
     avatar              = db.relationship('Photo', uselist=False)
-
     reviews             = db.relationship('Review',
                             backref=db.backref('provider'))
-    
     menus               = db.relationship('Menu',
                             backref=db.backref('provider'))
-    
     gallery             = db.relationship('Gallery', uselist=False)
+    products            = db.relationship('Product', backref='provider')
+
+
+class Consumer(db.Model):
+    id                  = db.Column(db.Integer, primary_key=True)
+    user                = db.relationship('User', backref='consumer',
+                            uselist=False)
+    hair_type           = db.Column(db.String)
+    hair_products       = db.relationship('Product', backref='consumer')
+    hair_routine        = db.Column(db.Text)
+    favorites           = db.relationship('Provider',
+                            backref=db.backref('favorited_by', lazy='dynamic'),
+                            secondary=consumers_providers)
+    links               = db.Column(db.String)
+    avatar              = db.relationship('Photo', uselist=False)
+    bio                 = db.Column(db.Text)
+    hair_articles       = db.relationship('Article')
+
+
