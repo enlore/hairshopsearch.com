4d579a3fb32bb8248a7ba3d4d59c4abaffc29510
enlore <n.e.lorenson@gmail.com>
2013-09-25 20:54:44 -0500

    Manage a app

--- manage.py
+++ manage.py
@@ -1,12 +1,24 @@
 #!/home/no/.venvs/hairshopsearch/bin/python
 from flask.ext.script import Manager, Server, Shell 
 from app import create_app
+from app.core import db, ud
+from app.user.models import (User, Role, Provider, Address, Photo, Review,
+                                Gallery)
 
 m = Manager(create_app)
 
 def _make_context():
     return dict(
            app=create_app(),
+           db=db,
+           ud=ud,
+           User=User,
+           Role=Role,
+           Provider=Provider,
+           Review=Review,  
+           Address=Address, 
+           Gallery=Gallery,
+           Photo=Photo
             )
 
 m.add_command('run', Server(port='9016'))
