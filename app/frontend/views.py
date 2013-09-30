1a9239f61a825e31876e17a0b884f34fc553f5a8
enlore <n.e.lorenson@gmail.com>
2013-09-30 12:14:25 -0500

    Routes

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
+from flask.ext.security import current_user, login_required
 from sqlalchemy import or_
 from ..user.models import Provider, Consumer
 
@@ -7,15 +8,42 @@ frontend = Blueprint('frontend', __name__, template_folder='templates')
 
 @frontend.route('/')
 def index():
-    return 'index yo'
+    return render_template('frontend/index.html')
+
+@frontend.route('/profile')
+@login_required
+def profile():
+    if current_user.consumer:
+        return render_template('frontend/consumer.html',
+                consumer=current_user.consumer)
+
+    if current_user.provider:
+        return render_template('frontend/provider.html',
+                provider=current_user.provider)
+
+@frontend.route('/test_provider')
+def test_provider():
+    provider = Provider.query.first()
+    return render_template('frontend/provider.html', provider=provider)
+
+@frontend.route('/test_consumer')
+def test_consumer():
+    consumer = Consumer.query.first()
+    return render_template('frontend/consumer.html', consumer=consumer)
+
+@frontend.route('/welcome')
+def welcome():
+    return render_template('frontend/welcome.html')
+
+@frontend.route('/new_provider', methods=['GET', 'POST'])
+def new_provider():
+    return 'new provider'
+
+@frontend.route('/new_consumer', methods=['GET', 'POST'])
+def new_consumer():
+    return 'new consumer'
 
 @frontend.route('/<provider_name>')
 def provider(provider_name):
-    provider = Provider.query.filter(or_(Provider.business_name==provider_name, Provider.id==provider_name))\
-                .first()
-    return render_template('frontend/provider.html', provider=provider)
-
-@frontend.route('/consumer/<consumer_id>')
-def consumer(consumer_id):
-    consumer = Consumer.query.filter(Consumer.id==consumer_id).first()
-    return render_template('frontend/consumer.html', consumer=consumer)
+    """TODO"""
+    return 'TODO'
