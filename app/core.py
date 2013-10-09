5ee37690dc7b50e1c92fdb62035c3f33218327cc
enlore <n.e.lorenson@gmail.com>
2013-10-09 12:59:20 -0500

    Clip ws, remove superfluous import

--- app/core.py
+++ app/core.py
@@ -1,10 +1,8 @@
 from flask.ext.sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 from flask.ext.security import SQLAlchemyUserDatastore
 from app.user.models import User, Role
-
 ud = SQLAlchemyUserDatastore(db, User, Role)
 
 from flask.ext.mail import Mail
