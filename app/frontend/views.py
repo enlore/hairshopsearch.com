1a4a8c56b8cf7fd5c6c4dec2350d3241c235d193
enlore <n.e.lorenson@gmail.com>
2013-10-15 20:31:18 -0500

    Provider frontend stuff

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,14 +1,24 @@
 # -*- encoding: utf-8 -*-
 from flask import (Blueprint, render_template, current_app, redirect, url_for,
-    flash)
+    flash, abort)
 from flask.ext.security import login_required
 from ..user.models import Provider, Consumer
+from ..search.forms import SearchForm
 
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
 @frontend.route('/')
 def index():
-    return render_template('frontend/index.html')
+    return render_template('frontend/index.html', search_form=SearchForm())
+
+@frontend.route('/<provider_url>')
+def provider_url(provider_url):
+    p = Provider.query.filter(Provider._business_url==provider_url).first()
+    current_app.logger.info(p)
+    if p:
+        return render_template('frontend/provider.html', provider=p)
+    else:
+        abort(404)
 
 @frontend.route('/test_provider')
 def test_provider():
