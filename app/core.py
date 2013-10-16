70a0fad21362d6a4d0cbb0803d6bf665382672cb
enlore <n.e.lorenson@gmail.com>
2013-10-15 20:36:13 -0500

    Exception class

--- app/core.py
+++ app/core.py
@@ -7,3 +7,10 @@ ud = SQLAlchemyUserDatastore(db, User, Role)
 
 from flask.ext.mail import Mail
 mail = Mail()
+
+from pyelasticsearch import ElasticSearch
+es = ElasticSearch('http://localhost:9200')
+
+class HSSError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
