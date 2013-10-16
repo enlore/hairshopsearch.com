938d996cc83f6d358b1904588bc9fc56f67ffc87
enlore <n.e.lorenson@gmail.com>
2013-10-15 20:41:55 -0500

    Lat/long and JSONEncoder

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,73 @@
+import requests
 import simplejson as json
+from pyelasticsearch.client import _iso_datetime
+from six import PY3
+
+def lat_log(accept_type, address, sensor='false'):
+    """Access the Google Geocoding API to return lat and lon given an address.
+
+    :param address: url encoded address string
+    :param accept_type: json or xml
+    :param sensor: 'false' or 'true': tells google api if loc data is
+        generated from a device sensor
+    :type sensor: string
+    :rtype: array of tuples of the format (lat, lon)
+    """
+
+    params = {}
+    params['address'] = address
+    params['sensor'] = sensor
+
+    resp = requests.get(geo_uri + accept_type, params=params)
+
+    decoded_resp = resp.json()
+
+    if decoded_resp['status'] == 'OK':
+        locs = []
+        for res in decoded_resp['results']:
+            locs.append(
+                    (res['geometry']['location']['lat'],
+                     res['geometry']['location']['lng'])
+                )
+        return locs
+
+    elif decoded_resp['REQUEST_DENIED']:
+        if decoded_resp['error_message']:
+            raise HSSError("GEOLOC req denied: %s" % decoded_resp['error_message'])
+        else:
+            raise HSSError("GEOLOC req denied")
+
+    elif decoded_resp['status'] == u'ZERO_RESULTS':
+        raise HSSError("We couldn't find your address.  Please try again.")
+
+    elif decoded_resp['status'] == 'UNKOWN_ERROR':
+        raise HSSError('Service error.  Try again later.')
+
+    elif decoded_resp['stats'] == 'OVER_QUERY_LIMIT':
+        raise HSSError('Over query limit.')
+
+    else:
+        raise HSSError(decoded_resp['status'])
+
+def acceptable_url_string(string, proto_string):
+    """Clean a string of all it's unnacceptable chars.
+
+    :param string: business name to be cleaned
+    :param proto_string: acceptable charset
+    """
+
+    res = []
+    for ch in (char for char in string.strip()):
+        # replace as single whitespace with a single hyphen
+        if ch == ' ':
+            ch = '-'
+        if ch in proto_string:
+            res.append(ch)
+
+    res_string = u''.join(res)
+    if res_string[-1] == '_':
+        return res_string[0:-1]
+    return res_string
 
 class JSONSerializer():
     """Smart mixin that tags a SQLAlchemy model as serializable"""
@@ -28,7 +97,20 @@ class JSONSerializer():
         return ret
 
 class JSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, JSONSerializer):
-            return obj.to_json()
-        return super(JSONEncoder, self).default(obj)
+    def default(self, value):
+        """Convert more Python data types to ES-understandable JSON."""
+        iso = _iso_datetime(value)
+        if iso:
+            return iso
+
+        if not PY3 and isinstance(value, str):
+            return unicode(value, errors='replace')  # TODO: Be stricter.
+
+        if isinstance(value, set):
+            return list(value)
+
+        if isinstance(value, JSONSerializer):
+            return value.to_json()
+
+        return super(JSONEncoder, self).default(value)
+
