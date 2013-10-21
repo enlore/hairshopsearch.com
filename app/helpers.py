9b369c25444eee22c90571be8da4292a58bbf479
enlore <n.e.lorenson@gmail.com>
2013-10-21 14:03:15 -0500

    Redoc, rework, reorg

--- app/helpers.py
+++ app/helpers.py
@@ -3,52 +3,6 @@ import simplejson as json
 from pyelasticsearch.client import _iso_datetime
 from six import PY3
 
-def lat_log(accept_type, address, sensor='false'):
-    """Access the Google Geocoding API to return lat and lon given an address.
-
-    :param address: url encoded address string
-    :param accept_type: json or xml
-    :param sensor: 'false' or 'true': tells google api if loc data is
-        generated from a device sensor
-    :type sensor: string
-    :rtype: array of tuples of the format (lat, lon)
-    """
-
-    params = {}
-    params['address'] = address
-    params['sensor'] = sensor
-
-    resp = requests.get(geo_uri + accept_type, params=params)
-
-    decoded_resp = resp.json()
-
-    if decoded_resp['status'] == 'OK':
-        locs = []
-        for res in decoded_resp['results']:
-            locs.append(
-                    (res['geometry']['location']['lat'],
-                     res['geometry']['location']['lng'])
-                )
-        return locs
-
-    elif decoded_resp['REQUEST_DENIED']:
-        if decoded_resp['error_message']:
-            raise HSSError("GEOLOC req denied: %s" % decoded_resp['error_message'])
-        else:
-            raise HSSError("GEOLOC req denied")
-
-    elif decoded_resp['status'] == u'ZERO_RESULTS':
-        raise HSSError("We couldn't find your address.  Please try again.")
-
-    elif decoded_resp['status'] == 'UNKOWN_ERROR':
-        raise HSSError('Service error.  Try again later.')
-
-    elif decoded_resp['stats'] == 'OVER_QUERY_LIMIT':
-        raise HSSError('Over query limit.')
-
-    else:
-        raise HSSError(decoded_resp['status'])
-
 def acceptable_url_string(string, proto_string):
     """Clean a string of all it's unnacceptable chars.
 
@@ -65,9 +19,61 @@ def acceptable_url_string(string, proto_string):
             res.append(ch)
 
     res_string = u''.join(res)
-    if res_string[-1] == '_':
+
+    if res_string[-1] == '-':
         return res_string[0:-1]
+
     return res_string
+
+class HSSError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def lat_lon(address, sensor='false'):
+    """Access the Google Geocoding API to return lat and lon given an address.
+
+    :param address: instance of :class:Address
+    :param sensor: 'false' or 'true': tells google api if loc data is
+        generated from a device sensor
+    :type sensor: string
+    :rtype: array of tuples of the format (lat, lon)
+    """
+
+    params = {}
+    params['address'] = str(address)
+    params['sensor'] = sensor
+
+    geo_uri = 'http://maps.googleapis.com/maps/api/geocode/json'
+    resp = requests.get(geo_uri, params=params)
+
+    decoded_resp = resp.json()
+
+    if decoded_resp['status'] == 'OK':
+        locs = []
+        for res in decoded_resp['results']:
+            locs.append(
+                    (res['geometry']['location']['lat'],
+                     res['geometry']['location']['lng'])
+                )
+        return locs
+
+    elif decoded_resp['status'] == 'REQUEST_DENIED':
+        if decoded_resp['error_message']:
+            raise HSSError("GEOLOC req denied: %s" % decoded_resp['error_message'])
+        else:
+            raise HSSError("GEOLOC req denied")
+
+    elif decoded_resp['status'] == u'ZERO_RESULTS':
+        raise HSSError("We couldn't find your address.  Please try again.")
+
+    elif decoded_resp['status'] == 'UNKOWN_ERROR':
+        raise HSSError('Service error.  Try again later.')
+
+    elif decoded_resp['stats'] == 'OVER_QUERY_LIMIT':
+        raise HSSError('Over query limit.')
+
+    else:
+        raise HSSError(decoded_resp['status'])
 
 class JSONSerializer():
     """Smart mixin that tags a SQLAlchemy model as serializable"""
