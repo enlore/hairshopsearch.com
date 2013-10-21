d15fabcaa29ce3870d2c00baa0724dc0a2af0b7c
enlore <n.e.lorenson@gmail.com>
2013-10-21 14:04:32 -0500

    Search form

--- app/search/forms.py
+++ app/search/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import Form
-from wtforms import TextField, SubmitField
+from wtforms import TextField, SubmitField, SelectField
 
+menu_types = [('barber', 'barber'), ('salon', 'salon')]
 class SearchForm(Form):
-    business_name   = TextField('')
+    menu_type       = SelectField('Type', choices=menu_types)
+    service         = TextField('Service')
+    zip_code        = TextField('Zip')
     submit          = SubmitField('Search')
