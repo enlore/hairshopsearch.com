f2b4b6a65593431f79575a59b987cc5b35d5b81f
enlore <n.e.lorenson@gmail.com>
2013-11-04 17:25:25 -0600

    Zip required for search

--- app/search/forms.py
+++ app/search/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
 from wtforms import TextField, SubmitField, SelectField
+from wtforms.validators import Required
 
 menu_types = [('barber', 'barber'), ('salon', 'salon')]
 class SearchForm(Form):
     menu_type       = SelectField('Type', choices=menu_types)
     service         = TextField('Service')
-    zip_code        = TextField('Zip')
+    zip_code        = TextField('Zip', [Required()])
     submit          = SubmitField('Search')
