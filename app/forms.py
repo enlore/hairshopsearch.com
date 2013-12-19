eccf5514455c3b0f358a22716a5d691263bb47e9
enlore <n.e.lorenson@gmail.com>
2013-12-18 22:25:07 -0600

    Hours on form

--- app/forms.py
+++ app/forms.py
@@ -125,6 +125,34 @@ def multi_checkboxes(field, ul_class=u'', **kwargs):
     html.append(Markup('</ul>'))
     return Markup('\n').join(html)
 
+times = [('closed', 'Closed'),
+        ('5:00am', '5:00am'), ('5:15am', '5:15am'), ('5:30am', '5:30am'),
+        ('5:45am', '5:45am'), ('6:00am', '6:00am'), ('6:15am', '6:15am'),
+        ('6:30am', '6:30am'), ('6:45am', '6:45am'), ('7:00am', '7:00am'),
+        ('7:15am', '7:15am'), ('7:30am', '7:30am'), ('7:45am', '7:45am'),
+        ('8:00am', '8:00am'), ('8:15am', '8:15am'), ('8:30am', '8:30am'),
+        ('8:45am', '8:45am'), ('9:00am', '9:00am'), ('9:15am', '9:15am'),
+        ('9:30am', '9:30am'), ('9:45am', '9:45am'), ('10:00am', '10:00am'),
+        ('10:15am', '10:15am'), ('10:30am', '10:30am'), ('10:45am', '10:45am'),
+        ('11:00am', '11:00am'), ('11:15am', '11:15am'), ('11:30am', '11:30am'),
+        ('11:45am', '11:45am'), ('12:00pm', '12:00pm'), ('12:15pm', '12:15pm'),
+        ('12:30pm', '12:30pm'), ('12:45pm', '12:45pm'), ('1:00pm', '1:00pm'),
+        ('1:15pm', '1:15pm'), ('1:30pm', '1:30pm'), ('1:45pm', '1:45pm'),
+        ('2:00pm', '2:00pm'), ('2:15pm', '2:15pm'), ('2:30pm', '2:30pm'),
+        ('2:45pm', '2:45pm'), ('3:00pm', '3:00pm'), ('3:15pm', '3:15pm'),
+        ('3:30pm', '3:30pm'), ('3:45pm', '3:45pm'), ('4:00pm', '4:00pm'),
+        ('4:15pm', '4:15pm'), ('4:30pm', '4:30pm'), ('4:45pm', '4:45pm'),
+        ('5:00pm', '5:00pm'), ('5:15pm', '5:15pm'), ('5:30pm', '5:30pm'),
+        ('5:45pm', '5:45pm'), ('6:00pm', '6:00pm'), ('6:15pm', '6:15pm'),
+        ('6:30pm', '6:30pm'), ('6:45pm', '6:45pm'), ('7:00pm', '7:00pm'),
+        ('7:15pm', '7:15pm'), ('7:30pm', '7:30pm'), ('7:45pm', '7:45pm'),
+        ('8:00pm', '8:00pm'), ('8:15pm', '8:15pm'), ('8:30pm', '8:30pm'),
+        ('8:45pm', '8:45pm'), ('9:00pm', '9:00pm'), ('9:15pm', '9:15pm'),
+        ('9:30pm', '9:30pm'), ('9:45pm', '9:45pm'), ('10:00pm', '10:00pm'),
+        ('10:15pm', '10:15pm'), ('10:30pm', '10:30pm'), ('10:45pm', '10:45pm'),
+        ('11:00pm', '11:00pm'), ('11:15pm', '11:15pm'), ('11:30pm', '11:30pm'),
+        ('11:45pm', '11:45pm'), ('12:00am', '12:00am')]
+
 
 class ProviderDashForm(Form):
     avatar          = TextField('Avatar')
@@ -145,5 +173,20 @@ class ProviderDashForm(Form):
     bio             = TextField('About Us')
     fb_url          = TextField('Facebook Page')
     twitter_url     = TextField('Twitter')
+
+    monday_open     = SelectField(u'Monday open', choices=times)
+    monday_close    = SelectField(u'Monday close', choices=times)
+    tuesday_open    = SelectField(u'Tuesday open',choices=times)
+    tuesday_close   = SelectField(u'Tuesday close',choices=times)
+    wednesday_open  = SelectField(u'Wednesday open', choices=times)
+    wednesday_close = SelectField(u'Wednesday close', choices=times)
+    thursday_open   = SelectField(u'Thursday open', choices=times)
+    thursday_close  = SelectField(u'Thursday close', choices=times)
+    friday_open     = SelectField(u'Friday open', choices=times)
+    friday_close    = SelectField(u'Friday close', choices=times)
+    saturday_open   = SelectField(u'Saturday open', choices=times)
+    saturday_close  = SelectField(u'Saturday close', choices=times)
+    sunday_open     = SelectField(u'Sunday open', choices=times)
+    sunday_close    = SelectField(u'Sunday close', choices=times)
 
     submit          = SubmitField('Save Changes')
